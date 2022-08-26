578ea0dbe7ffd81eeadd57c1f87b5ab36246d73c
mdmarjanhossain0 <mdmarjanhossain0@gmail.com>
2022-08-27 02:55:45 +0600

    initial commit

---
+++ js/navbar.js
@@ -0,0 +1,15 @@
+var checkbox = document.getElementById("side-menu-bar")
+var sidebar = document.getElementById("sidebar")
+var maincontent = document.getElementById("main-content")
+
+checkbox.addEventListener('change', function () {
+    if (this.checked) {
+        sidebar.classList.add("collaps-side-bar")
+        maincontent.classList.add("collaps-main-content")
+        console.log("Checkbox is checked..");
+    } else {
+        sidebar.classList.remove("collaps-side-bar")
+        maincontent.classList.remove("collaps-main-content")
+        console.log("Checkbox is not checked..");
+    }
+});
